d9794eb540398492e3b8661a2a5a3c02766c90e0
TinyChou <zhoujinfu0124@163.com>
2022-05-26 00:25:10 +0800

    feat: divide support to `module-a`

---
+++ .changeset/strange-meals-develop.md
@@ -0,0 +1,5 @@
+---
+"@tinychou/module-a": patch
+---
+
+A patch from branch `feat/feature-test-2` with `divide` support to `module-a`
--- packages/module-a/src/index.ts
+++ packages/module-a/src/index.ts
@@ -3,3 +3,7 @@ export {}
 export function add(a: number, b: number): number {
   return a + b
 }
+
+export function divide(a: number, b: number): number {
+  return a / b
+}
