1fc16447f00ad3991afb4aa0d0b99565ff481501
TinyChou <zhoujinfu0124@163.com>
2022-05-26 00:15:41 +0800

    feat: multiple support to `module-a`

--- packages/module-a/src/index.ts
+++ packages/module-a/src/index.ts
@@ -7,3 +7,7 @@ export function add(a: number, b: number): number {
 export function minus(a: number, b: number): number {
   return a - b
 }
+
+export function multiple(a: number, b: number): number {
+  return a * b
+}
